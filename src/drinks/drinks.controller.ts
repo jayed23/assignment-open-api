1cd8c211ae4e32b049515477918e71477bc9a302
Your Name <justinejade.sulo@cvsu.edu.ph>
2025-01-12 22:11:57 +0800

    getbydrinkname fucntion

--- src/drinks/drinks.controller.ts
+++ src/drinks/drinks.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { DrinksService } from './drinks.service';
 
 @Controller('drinks')
-export class DrinksController {}
+export class DrinksController {
+  constructor(private readonly drinksService: DrinksService) {}
+
+  @Get('by-name')
+  async getDrinksByName(@Query('name') name: string) {
+    return this.drinksService.getDrinksByName(name);
+  }
+}
